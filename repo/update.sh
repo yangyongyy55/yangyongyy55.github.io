0796c54864c2e5d83025b7d5df0a6c80c2721368
yangyongyy55 <yangyongyy55@gmail.com>
2025-02-10 10:24:32 +0300

    更新脚本

--- repo/update.sh
+++ repo/update.sh
@@ -1,9 +1,12 @@
-#!/bin/sh
+#!/bin/bash
+
+# 依赖 sudo apt update && sudo apt install dpkg-dev
+
 
 rm -rf Packages Packages.gz Packages.bz2
 
 # 生成新的package
 dpkg-scanpackages debs /dev/null > Packages
 tar zcvf Packages.gz Packages
-bzip2 -k Packages Packages.bz2
+bzip2 -k Packages
 
