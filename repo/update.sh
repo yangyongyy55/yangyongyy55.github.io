350f9ed38cadfea077cdb5e5aecb4bb67cff2259
yangyongyy55 <yangyongyy55@gmail.com>
2025-06-24 09:26:04 +0300

    支持多版本

--- repo/update.sh
+++ repo/update.sh
@@ -6,7 +6,7 @@
 rm -rf Packages Packages.gz Packages.bz2
 
 # 生成新的package
-dpkg-scanpackages debs /dev/null > Packages
+dpkg-scanpackages -m debs /dev/null > Packages
 tar zcvf Packages.gz Packages
 bzip2 -k Packages
 
